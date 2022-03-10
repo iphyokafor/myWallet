acf4784f92b0a703f893e42944dec97f82d2a880
iphyokafor <sandraifeoma22@gmail.com>
2022-03-10 03:09:01 -0800

    refactor wallet contract

--- myWallet.sol
+++ myWallet.sol
@@ -2,57 +2,67 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract MyWallet {
+    //Events
+    event Deposit(address _addr, uint256 amount);
+    event Send(address _addr, uint256 amount);
+    event Withdraw(address _addr, uint256 amount);
 
-    struct Payment {
-        uint amount;
-        uint timestamp;
-    }
+    address public owner;
 
-    struct Balance {
-        uint totalBalance;
-        uint numPayments;
-        mapping(uint => Payment) payments;
-    }
-
-
-address public owner;
-constructor(){
+    constructor() {
         owner = msg.sender;
     }
 
-    mapping(address => Balance) public balanceReceived;
+    mapping(address => uint256) public balanceReceived;
 
-     modifier onlyOwner(){
+    // Modifiers
+    modifier hasEnough(address _addr, uint256 _amount) {
+        require(balanceReceived[_addr] >= _amount, "Insufficient funds");
+        _;
+    }
+
+    modifier onlyOwner() {
         _;
         require(owner == msg.sender, "Only owner can call this function");
         _;
     }
 
+    //Receive ether
+    receive() external payable {
+        balanceReceived[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
 
-    function getBalance() public view returns(uint) {
+    //Send money
+    function sendMoney(address _receiver, uint256 _amount)
+        external
+        payable
+        onlyOwner
+        hasEnough(owner, _amount)
+    {
+        balanceReceived[_receiver] += _amount;
+        balanceReceived[msg.sender] -= _amount;
+        emit Send(_receiver, _amount);
+    }
+
+    // Withdraw money
+    function withdrawMoney(address payable _to, uint256 _amount)
+        public
+        onlyOwner
+        hasEnough(owner, _amount)
+    {
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    // Get contract balance address
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function sendMoney() external onlyOwner payable {
-        balanceReceived[msg.sender].totalBalance += msg.value;
-
-        Payment memory payment = Payment(msg.value, block.timestamp);
-        balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments] = payment;
-        balanceReceived[msg.sender].numPayments++;
-        
-    }
-
-    function withdrawMoney(address payable _to, uint _amount) public {
-        require(_amount <= balanceReceived[msg.sender].totalBalance, "not enough funds");
-        balanceReceived[msg.sender].totalBalance -= _amount;
-        _to.transfer(_amount);
-        
-
-    }
-
-    function withdrawAllMoney(address payable _to) public {
-        uint balanceToSend = balanceReceived[msg.sender].totalBalance;
-        balanceReceived[msg.sender].totalBalance = 0;
-        _to.transfer(balanceToSend);
+    // Get balance with specific address
+    function getBalance(address _addr) external view returns (uint256) {
+        return balanceReceived[_addr];
     }
 }
